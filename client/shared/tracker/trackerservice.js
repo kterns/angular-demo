50436ace7f83e6d7f5f05910e841a64fc8b2ccc1
Kyle <ternsk2@winthrop.edu>
2016-03-01 12:09:12 -0500

    Updated JSON url

--- client/shared/tracker/trackerservice.js
+++ client/shared/tracker/trackerservice.js
@@ -2,8 +2,6 @@
 
 app.service('trackersService', function($http) {
 	this.getData = function() {
-		return $http.get("http://localhost:8079/assets/downloads/resources.json");
-		// return $http.get("http://192.168.0.23/sitetracker/assets/downloads/resources.json");
-		// return $http.get("http://kterns.com/sitetracker/assets/downloads/resources.json");
+		return $http.get("http://kterns.com/angular-demo/assets/downloads/resources.json");
 	}
 });
